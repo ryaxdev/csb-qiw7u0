e8a6cd48a6dacc43e46c06f71e8e3f7cbc3382fc
Code Sandbox <hello@codesandbox.io>
2022-08-18 15:46:36 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b39a4872402845f7dfbed0d733909543.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "489384f0a322fb1364b1306401621121",
+    "url": "https://ryaxdev.github.io/csb-qiw7u0/index.html"
+  },
+  {
+    "revision": "c96b3ff1943675ac0f35",
+    "url": "https://ryaxdev.github.io/csb-qiw7u0/static/js/2.8273aae6.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "https://ryaxdev.github.io/csb-qiw7u0/static/js/2.8273aae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7ef169be1a83d373ac5",
+    "url": "https://ryaxdev.github.io/csb-qiw7u0/static/js/main.44c47536.chunk.js"
+  },
+  {
+    "revision": "f77df0364d47fed9765b",
+    "url": "https://ryaxdev.github.io/csb-qiw7u0/static/js/runtime-main.a2d85548.js"
+  }
+]);
